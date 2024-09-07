0bc926d3cac7252fcbc8ffd4f3efec3d7c6a5ae3
fortport48 <96012263+fortport48@users.noreply.github.com>
2024-09-07 23:24:22 +0530

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -1,6 +1,6 @@
-const loginForm = document.getElementById("index");
-const loginButton = document.getElementById("index-submit");
-const loginErrorMsg = document.getElementById("index-msg");
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
