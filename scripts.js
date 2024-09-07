29bafefff266d090c154c2e43f7e7bfeb2655a8c
fortport48 <96012263+fortport48@users.noreply.github.com>
2024-09-07 22:26:11 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,6 +12,7 @@ function validateForm() {
     // For demonstration, check if the username is 'admin' and password is 'password'
     if (username === "admin" && password === "password") {
         alert("Login successful!");
+         window.location.href = "welcome.html";
         return true;
     } else {
         errorMessage.textContent = "Invalid Username or Password!";
