c1cc7dc166860ec53931008c6f5406cbbbfdac91
fortport48 <96012263+fortport48@users.noreply.github.com>
2024-09-10 19:52:14 +0530

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -9,7 +9,7 @@ loginButton.addEventListener("click", (e) => {
 
     if (username === "user" && password === "web_dev") {
         alert("You have successfully logged in.");
-        location.reload();
+        window.location.replace("/update.html")
     } else {
         loginErrorMsg.style.opacity = 1;
     }
